0948b0792e34ccc4e9c56b83378419a864617234
Dhruv <dhruvpatel5738@gmail.com>
2020-04-24 17:50:16 -0700

    Entire Project

---
+++ README.md
@@ -0,0 +1,52 @@
+## AuraPlayer: Mood based playlist generator
+
+![Logo](https://github.com/dhruvp-8/aura-player/blob/master/screenshots/Logo.png "Logo")
+
+Creators:
+
+1. Dhruv Patel: Spotify API integration module
+2. Yash Shah: IBM Watson NLU API module
+3. Santhosh Narayanan: Web Application and UI designing
+4. Yash Patel: Web Application and UI designing
+
+## Introduction
+* The system was built during University of Southern California, ACM TrojanHacks held on 14-15th October 2018. 
+* AuraPlayer enables the user to interact by describing how the user feels, based upon the user's voice response, the system generates a curated playlist of 12 songs dependent on user's emotion/mood.
+* The system is built upon IBM Watson's Natural Language Understanding API for identifying the sentiment score of the user's voice input such as happy, sad, anger, disgust.
+* The score is then fed as a seed to the Spotify's API to generate the desired playlist
+
+### Prerequisites
+
+
+### Installing
+
+
+## Modules
+
+
+
+
+## Output
+
+![Walkthrough](https://media.giphy.com/media/dJQUmfBnL19C8mg5bR/giphy.gif "Walkthrough")
+
+
+## Running the tests
+
+
+
+## Deployment
+
+
+## Versioning
+
+
+
+## Authors
+
+
+
+## License
+
+
+## Acknowledgments
---
+++ server.js
@@ -0,0 +1,113 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var morgan = require('morgan');
+var bodyParser = require('body-parser')
+var cors = require('cors');
+var request = require('request');
+
+var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+var natural_language_understanding = new NaturalLanguageUnderstandingV1({
+	'username': 'YOUR_USERNAME',
+	'password': 'YOUR_PASSWORD',
+	'version': '2018-03-16'
+});
+
+
+var client_id = 'YOUR_CLIENT_ID'; // Your client id
+var client_secret = 'YOUR_CLIENT_SECRET';
+
+var port = process.env.PORT || 5000;
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/process', function(req,res){
+	text = req.body.desc
+	console.log(text)
+	var parameters = {
+	  'text': text,
+	  'features': {
+	    'entities': {
+	      'emotion': true,
+	      'sentiment': true,
+	      'limit': 1
+	    },
+	    'keywords': {
+	      'emotion': true,
+	      'sentiment': true,
+	      'limit': 1
+	    }
+	  }
+	}
+	var emotions = []
+	var sentiment = []
+	natural_language_understanding.analyze(parameters, function(err, response) {
+	  if (err){
+	    console.log('error:', err);
+		res.json({'flag': false})
+	  }
+	  else{
+		    //var fs = require("fs")
+		    //var contents = fs.readFileSync('jsoncontents.')
+		    console.log(response)
+		    //console.log(response.keywords)
+		    if(response.keywords){
+		    	if(response.keywords.length != 0){
+			    	for(var i=0; i < response.keywords.length; i++){
+			    		if(response.keywords[i].emotion != null || response.keywords[i].sentiment != null){
+			    			emotions.push(response.keywords[i].emotion)
+			    			sentiment.push(response.keywords[i].sentiment)
+			    		}
+			    		else{
+			    			res.json({'flag': false})
+			    		}
+			    	}
+			    	res.json({'emotions': emotions, 'sentiment': sentiment, 'flag': true}) 
+		    	}
+		    	else{
+		    		res.json({'flag': false})
+		    	}
+		    }
+		    else{
+		    	res.json({'err': 'Please speak a bigger sentence.', 'flag': false})
+		    }
+	   	}
+	    
+	  
+	});
+});
+
+
+app.get('/token', function(req, res){
+	request.post(authOptions, function(error, response, body) {
+	  if (!error && response.statusCode === 200) {
+
+	    // use the access token to access the Spotify Web API
+	    var token = body.access_token;
+	    res.json({'access_token': token});
+	  }
+	});
+});
+
+
+app.listen(port, function(){
+    console.log('Server running at port ' + port);
+});
